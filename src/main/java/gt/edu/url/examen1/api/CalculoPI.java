1dfe5ad8ea5c7d645c7a87eaa9eefda188b999d9
pabloaquino <31853567+pabloaquino@users.noreply.github.com>
2017-09-11 22:57:32 -0600

    Aun me cuesta entender las reglas de java

---
+++ src/main/java/gt/edu/url/examen1/api/CalculoPI.java
@@ -0,0 +1,36 @@
+package gt.edu.url.examen1.api;
+
+public class CalculoPI {
+
+	int repeticiones;
+	public CalculoPI(int k) 
+	{
+		repeticiones = k;
+	}
+	public  double calcularPi(int k)
+	{       
+	    if(k==0)
+	        return Pi(k);
+	    else {
+	        double resultado= calcularPi(k)+ calcularPi(k-1);
+	        return resultado;
+	    }
+
+	}
+
+	public double Pi(int k)
+	{
+	    double numerador=(factorial(6*k)*((545140134*k)+13591409));
+	    double denominador =(factorial(3*k)*Math.pow(factorial(k), 3)*Math.pow(-640320, (3*k)));
+	    double Pi=(numerador/denominador);
+	    return Pi;
+	}
+
+	 public double factorial(int n)// This is a class to calculate an factorial of a number
+	 {
+	    if (n==0)
+	       return 1;
+	    else
+	       return n*(factorial(n-1));
+	}
+}
--- src/main/java/gt/edu/url/examen1/impl/Main.java
+++ src/main/java/gt/edu/url/examen1/impl/Main.java
@@ -1,13 +1,33 @@
 package gt.edu.url.examen1.impl;
 
+import java.util.Scanner;
+
+import gt.edu.url.examen1.api.CalculoPI;
+import gt.edu.url.examen1.api.Chudnovsky;
 /**
  *
  * @author tuxtor
  */
 public class Main {
-    
+	static CalculoPI miclase = new CalculoPI(0);
+	
     public static void main(String[] args) {
-        System.out.println("Examen 2");
+    	System.out.println("Examen 2");
+    	Scanner Sc = new Scanner(System.in);
+    	System.out.println("Ingrese el numero de ciclos para calcular el valor de PI");
+    	int k = 0;
+    	k = Sc.nextInt();
+    	Main main = new Main();
+    	main.Iniciar(k);
+    	
+    	miclase.calcularPi(k);
+    	
+    	System.out.println(miclase.calcularPi(k));
+    	
+    }
+    
+    public void Iniciar(int k) {
+    	CalculoPI pi = new CalculoPI(k);    
     }
     
 }
